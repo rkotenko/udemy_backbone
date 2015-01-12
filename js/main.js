03d0001198b087dc3d1c7f6b293e97c8abd78b5a
rkotenko <robkotenko@gmail.com>
2015-01-11 19:21:36 -0500

    nothing much, just working through stuff

--- js/main.js
+++ js/main.js
@@ -2,3 +2,10 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+var Animal = Backbone.Model.extend({
+    walk: function() {
+        console.log('Animal walking');
+    }
+});
+
