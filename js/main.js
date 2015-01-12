d1c8d0afc1efd2f47e0e9466a9b90a7d46f7f448
rkotenko <robkotenko@gmail.com>
2015-01-12 11:44:39 -0500

    Merge branch 'collectionLesson' into master

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 
-// Mini project for the models section of the course
+// Mini project for the collection section of the course
 
 var Vehicle = Backbone.Model.extend({
     validate: function (attrs) {
-        if(!attrs.registrationNumber) {
+        if(!attrs.regNumber) {
             return 'Registration number is required';
         }
     },
@@ -13,13 +13,25 @@ var Vehicle = Backbone.Model.extend({
     urlRoot: '/api/vehicles'
 });
 
-var Car = Vehicle.extend({
-    start: function () {
-        console.log('Car started with registration number ' + this.get('registrationNumber'));
-    }
+var VehicleCollection = Backbone.Collection.extend({
+    model: Vehicle
 });
 
-var car = new Car({
-    registrationNumber: 'XLI887',
-    color: 'Red'
-});
+var vehicles = new VehicleCollection([
+    new Vehicle({regNumber: 'xli887', color: 'Blue'}),
+    new Vehicle({regNumber: 'znp123', color: 'Blue'}),
+    new Vehicle({regNumber: 'zuv456', color: 'Gray'})
+]);
+
+var blueCars = vehicles.where({color: 'Blue'});
+console.log('Blue cars: ' + blueCars);
+
+var oneCar = vehicles.findWhere({regNumber: 'xli887'});
+console.log('xli887: ' + oneCar);
+vehicles.remove(oneCar);
+console.log(vehicles.toJSON());
+vehicles.each(function (car) {
+    console.log(car);
+})
+
+
