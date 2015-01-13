d452c7f771a2bda7039379e21a1cbf6b2314f00e
Rob <robkotenko@gmail.com>
2015-01-13 17:02:20 -0500

    Finished the view project.  Moving on to the events section.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,50 @@
-// temp holding space for code I might need to use for each lesson
-var Song = Backbone.Model.extend({
-    defaults: {
-        plays: 10001
-    }
+var Vehicle = Backbone.Model.extend();
+
+var Vehicles = Backbone.Collection.extend({
+	model: Vehicle
 });
 
-var SongView = Backbone.View.extend({
-    initialize: function () {
-        this.model.on("change", this.render, this);
-    },
-    render: function () {
-        var template = _.template($('#songTemplate').html());
-		var html = template(this.model.toJSON());
-		this.$el.html(html);
-
-        return this;
-    }
+var VehicleView = Backbone.View.extend({
+	template: _.template($('#vehicleTemplate').html()),
+	events: {
+		'click.delete': 'delete'
+	},
+	delete: function() {
+		// destroy the model and then remove the view
+		this.model.destroy();
+		this.remove();
+		console.log('removed a view');
+	},
+	render: function() {
+		this.$el.html(this.template(this.model.toJSON())); // creates and inserts html using the template and a json version of the model data
+		return this;
+	}
 });
 
+var VehiclesView = Backbone.View.extend({
+	el: '#vehicles', // render this view at this html element,
+	initialize: function() {
+		this.listenTo(this.model, 'add', this.addOne);
+	},
+	// no corresponding button on page.  I created this just to play a bit with events.
+	addOne: function(vehicle) {
+		var vehicleView = new VehicleView({model: vehicle});
+		this.$el.append(vehicleView.render().$el);
+	},
+	render: function() {
+		var self = this; // set to ensure that inside of the .each, append is referring to the VehiclesView, not the vehicle
+		this.model.each(function(vehicle) {
+			var vehicleView = new VehicleView({model: vehicle}); // model contains the data for the view
+			self.$el.append(vehicleView.render().$el);
+		});
+	}
+});
 
-var song = new Song({title: "Blue is Green"});
+var vehicles = new Vehicles([
+	new Vehicle({regNum: 'abc123', color: 'blue'}),
+	new Vehicle({regNum: 'xyz123', color: 'red'})
+]);
 
-var songView = new SongView({el: '#songs', model: song});
-songView.render();
+var vehiclesView = new VehiclesView({model: vehicles}); // model is a collection, a collection of vehicles
+vehiclesView.render();
 
