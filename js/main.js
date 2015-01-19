15b50b31609e35b2767e18c3740dab8c838d5a62
rkotenko <robkotenko@gmail.com>
2015-01-19 14:17:06 -0500

    done with router content

--- js/main.js
+++ js/main.js
@@ -1,51 +1,72 @@
-var Vehicle = Backbone.Model.extend();
-
-var Vehicles = Backbone.Collection.extend({
-	model: Vehicle
-});
-
-var VehicleView = Backbone.View.extend({
-	template: _.template($('#vehicleTemplate').html()),
-	events: {
-		'click.delete': 'delete'
-	},
-	render: function() {
-		this.$el.html(this.template(this.model.toJSON())); // creates and inserts html using the template and a json version of the model data
+var ArtistsView = Backbone.View.extend({
+	render: function () {
+		this.$el.html('Artists view!');
+		
 		return this;
 	}
 });
 
-VehicleView.prototype.delete =  function() {
-		// destroy the model and then remove the view
-		this.model.destroy();
-		this.remove();
-		console.log('removed a view');
-};
+var AlbumsView = Backbone.View.extend({
+	render: function () {
+		this.$el.html('Albums view!');
 
-var VehiclesView = Backbone.View.extend({
-	el: '#vehicles', // render this view at this html element,
-	initialize: function() {
-		this.listenTo(this.model, 'add', this.addOne);
-	},
-	// no corresponding button on page.  I created this just to play a bit with events.
-	addOne: function(vehicle) {
-		var vehicleView = new VehicleView({model: vehicle});
-		this.$el.append(vehicleView.render().$el);
-	},
-	render: function() {
-		var self = this; // set to ensure that inside of the .each, append is referring to the VehiclesView, not the vehicle
-		this.model.each(function(vehicle) {
-			var vehicleView = new VehicleView({model: vehicle}); // model contains the data for the view
-			self.$el.append(vehicleView.render().$el);
-		});
+		return this;
 	}
 });
 
-var vehicles = new Vehicles([
-	new Vehicle({regNum: 'abc123', color: 'blue'}),
-	new Vehicle({regNum: 'xyz123', color: 'red'})
-]);
+var GenresView = Backbone.View.extend({
+	render: function () {
+		this.$el.html('Genres view!');
 
-var vehiclesView = new VehiclesView({model: vehicles}); // model is a collection, a collection of vehicles
-vehiclesView.render();
+		return this;
+	}
+});
 
+var AppRouter = Backbone.Router.extend({
+	routes: {
+		"albums": 'viewAlbums',
+		"albums/:albumId": 'viewAlbumById',
+		"artists": 'viewArtists',
+		"genres": 'viewGenres',
+		"*other": "defaultRoute"
+	},
+	
+	viewAlbumById: function (albumId) {
+		
+	},
+	
+	viewAlbums: function () {
+		var view = new AlbumsView({el: '#container'});
+		view.render();
+	},
+
+	viewArtists: function () {
+		var view = new ArtistsView({el: '#container'});
+		view.render();
+	},
+
+	viewGenres: function () {
+		var view = new GenresView({el: '#container'});
+		view.render();
+	},
+	
+	defaultRoutes: function () {
+		
+	}
+});
+
+var router = new AppRouter();
+Backbone.history.start();
+
+var NavView = Backbone.View.extend({
+	events: {
+		'click': "onClick"
+	},
+	
+	onClick: function (e) {
+		var $li = $(e.target);
+		router.navigate($li.attr("data-url"), {trigger: true});
+	}
+});
+
+var navView = new NavView({el: "#nav"});
